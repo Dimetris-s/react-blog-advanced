b22720b4811451067a4810c86facba4ac9801ab2
DESKTOP-INE9M8E\User <dim3triss@gmail.com>
2022-09-21 22:03:01 +0400

    Added babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,25 +1,34 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-  const {isDev} = options
+  const { isDev } = options;
+  const babelLoader = {
+    test: /\.(jsx?|tsx)$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      },
+    },
+    exclude: /node_modules/,
+  };
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: ['ts-loader'],
-    exclude: /node_modules/
-  }
+    exclude: /node_modules/,
+  };
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2?)$/,
-    use: ['file-loader']
-  }
+    use: ['file-loader'],
+  };
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
-  }
+  };
 
-  const cssLoader:webpack.RuleSetRule = {
+  const cssLoader: webpack.RuleSetRule = {
     test: /\.s[ac]ss$/,
     use: [
       isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
@@ -28,17 +37,18 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
         options: {
           modules: {
             auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-            localIdentName: isDev ? '[name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
-          }
-        }
+            localIdentName: isDev ? '[name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+          },
+        },
       },
-      'sass-loader'
-    ]
-  }
+      'sass-loader',
+    ],
+  };
   return [
+    babelLoader,
     typescriptLoader,
     cssLoader,
     svgLoader,
-    fileLoader
-  ]
+    fileLoader,
+  ];
 }
